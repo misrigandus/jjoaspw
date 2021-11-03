4388601df0ae602b3aa8d26647bd85ef5348c19c
misrigandus <93632948+misrigandus@users.noreply.github.com>
2021-11-03 10:25:59 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -8,3 +8,14 @@ sudo ufw allow 'Squid'
 #
 sudo ufw status
 #
+sudo cp /etc/squid/squid.conf{,.orginal}
+#
+sudo nano /etc/squid/squid.conf
+#
+sudo nano /etc/squid/allowSourceIP.txt
+#
+sudo systemctl restart squid
+#
+sudo apt-get install apache2-utils
+#
+sudo touch /etc/squid/squid_usr
