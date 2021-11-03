98a38a19c49adc9f9f2182b38c2b73130daee722
misrigandus <93632948+misrigandus@users.noreply.github.com>
2021-11-03 12:04:59 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -2,29 +2,29 @@
 
 sudo apt update
 #
-sudo apt install squid 
+sudo apt install squid -y
 #
-sudo systemctl status squid 
+sudo systemctl status squid -y
 #
-sudo ufw allow 'enable' 
+sudo ufw allow 'enable' -y
 #
-sudo ufw status 
+sudo ufw status -y
 #
-sudo nano /etc/squid/squid.conf 
+sudo nano /etc/squid/squid.conf -y
 #
-sudo nano /etc/squid/allowSourceIP.txt 
+sudo nano /etc/squid/allowSourceIP.txt -y
 #
 sudo systemctl restart squid -y
 #
-sudo apt-get install apache2-utils 
+sudo apt-get install apache2-utils -y
 #
-sudo touch /etc/squid/squid_usr 
+sudo touch /etc/squid/squid_usr -y
 #
-sudo chown proxy /etc/squid/squid_usr 
+sudo chown proxy /etc/squid/squid_usr -y
 #
-sudo dpkg -L squid | grep ncsa_auth 
+sudo dpkg -L squid | grep ncsa_auth -y
 #
-sudo systemctl restart squid 
+sudo systemctl restart squid -y
 #
 curl -v -x socks5://217.61.128.65:54470 https://github.com/VerusCoin/nheqminer/
 #
